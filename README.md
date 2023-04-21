9cc7b55ec8795087445e1d9bb3fb51166e29b485
jorgen-simonsson <jorgen.simonsson@gmail.com>
2023-04-21 21:40:33 +0200

    Complte with button step

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ Application to control 8 led lights from a rasberry py
 # Used HW
 
 See: https://www.mouser.se/datasheet/2/1376/8_MOSFETS_UsersGuide-3003107.pdf
+
+# Pusch button to be connected to pin 37 of the Raspberry Pi GPIO connector (GPIO26)
--- src/ring.py
+++ src/ring.py
@@ -2,19 +2,20 @@
 ''' App to handle 8 led lights '''
 import time
 import lib8mosind
+import RPi.GPIO as gpio
 
 
-
+BUTTON_CHANNEL = 26
 FIRST_CARD = 0
 SEC_CARD = 2
 ON = 1
 OFF = 0
 
+
 class Led:
     def __init__(self, card, led_number ):
         self.card = card
         self.led_number = led_number
-
 
 class Light:
     def __init__(self):
@@ -29,6 +30,28 @@ class Light:
         self.lights.append(Led(SEC_CARD,4))
         self.light_index = 8
 
+        # Init the button port as input with pull up
+        gpio.setmode(gpio.BCM)
+        gpio.setup(BUTTON_CHANNEL, gpio.IN, pull_up_down=gpio.PUD_UP)
+
+
+    def debounced_state(self):
+        counter = 0
+        state = gpio.input(BUTTON_CHANNEL)
+        # wait for 50ms
+        time.sleep(0.05)
+        # check the input pin 40 times in 1ms intervals
+        for i in range(40):
+            if gpio.input(BUTTON_CHANNEL) != state:
+                counter = 0
+                state = gpio.input(BUTTON_CHANNEL)
+            else:
+                counter += 1
+            time.sleep(0.001)
+            # if the input has been stable for 20ms, return the value
+            if counter >= 20:
+                return state
+
     def step(self):
         self.all_off()
         self.light_index = self.light_index +1
@@ -42,7 +65,7 @@ class Light:
         lib8mosind.set_all(SEC_CARD, 0)
 
     def show_off(self):
-        show_cnt=10
+        show_cnt=2
         self.all_off()
         index = 0
         while show_cnt >0:
@@ -53,10 +76,15 @@ class Light:
                 self.all_off()
                 index = index+ 1
             show_cnt = show_cnt -1
+        self.all_off()
 
 light = Light()
 light.show_off()
+old_state = light.debounced_state()
 while True:
-    light.step()
-    time.sleep(1)
+    new_state = light.debounced_state()
+    if new_state != old_state:
+        if new_state == 1:
+            light.step()
+    old_state = new_state
 
